5874b30cd0c01bb4599c75af9440ee12d9ac2331
JeremyCauper <jeremycauper@gmail.com>
2025-02-11 11:38:40 -0500

    Subida de nueva estructura de base de datos

--- storage/bd/incidencias_prueba.sql
+++ storage/bd/incidencias_prueba.sql
@@ -11,7 +11,7 @@
  Target Server Version : 100425
  File Encoding         : 65001
 
- Date: 06/02/2025 09:39:21
+ Date: 11/02/2025 10:49:39
 */
 
 SET NAMES utf8mb4;
@@ -1186,7 +1186,7 @@ CREATE TABLE `tb_menu`  (
   `updated_at` timestamp(0) NULL DEFAULT NULL,
   `created_at` timestamp(0) NOT NULL DEFAULT current_timestamp(0),
   PRIMARY KEY (`id_menu`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of tb_menu
@@ -1197,6 +1197,8 @@ INSERT INTO `tb_menu` VALUES (3, 'Visitas', 'fas fa-person-biking', 'ControlVisi
 INSERT INTO `tb_menu` VALUES (4, 'Empresas', 'far fa-building', 'ControlEmpresas', 1, 0, 1, NULL, '2025-02-01 18:12:14');
 INSERT INTO `tb_menu` VALUES (5, 'Control de Usuarios', 'fas fa-user-group', 'ControlUsarios', 1, 0, 1, NULL, '2025-02-01 18:14:02');
 INSERT INTO `tb_menu` VALUES (6, 'Mantenimientos', 'fas fa-gears', 'ControlMantenimientos', 1, 0, 1, NULL, '2025-02-01 18:15:04');
+INSERT INTO `tb_menu` VALUES (7, 'Sistema', 'fas fa-laptop-code', 'Sistema', 1, 0, 1, NULL, '2025-02-10 15:14:55');
+INSERT INTO `tb_menu` VALUES (8, 'Buzon Tecnico', 'fas fa-address-book', 'Buzon', 1, 0, 1, NULL, '2025-02-10 16:25:23');
 
 -- ----------------------------
 -- Table structure for tb_orden_correlativo
@@ -1326,13 +1328,13 @@ CREATE TABLE `tb_submenu`  (
 INSERT INTO `tb_submenu` VALUES (1, 3, 'Visitas', NULL, '/visitas/sucursales', 0, 1, '2025-02-01 20:09:44', '2025-01-31 21:06:39');
 INSERT INTO `tb_submenu` VALUES (2, 3, 'Terminadas', NULL, '/visitas/terminadas', 0, 1, '2025-02-01 20:06:11', '2025-01-31 21:06:39');
 INSERT INTO `tb_submenu` VALUES (3, 4, 'Empresas', NULL, '/empresas/empresas', 0, 1, NULL, '2025-02-01 20:12:21');
-INSERT INTO `tb_submenu` VALUES (4, 4, 'Grupos Empresas', 'Lol', '/empresas/grupos', 0, 1, '2025-02-05 11:59:39', '2025-02-01 20:13:05');
+INSERT INTO `tb_submenu` VALUES (4, 4, 'Grupos Empresas', NULL, '/empresas/grupos', 0, 1, '2025-02-10 10:20:20', '2025-02-01 20:13:05');
 INSERT INTO `tb_submenu` VALUES (5, 4, 'Sucursales Empresas', NULL, '/empresas/sucursales', 0, 1, NULL, '2025-02-01 20:13:33');
 INSERT INTO `tb_submenu` VALUES (6, 5, 'Usuarios', NULL, '/control-de-usuario/usuarios', 0, 1, NULL, '2025-02-01 20:15:25');
 INSERT INTO `tb_submenu` VALUES (7, 6, 'Problemas', 'Incidentes', '/mantenimiento/problemas/problemas', 0, 1, NULL, '2025-02-01 20:16:46');
 INSERT INTO `tb_submenu` VALUES (8, 6, 'Sub Problemas', 'Incidentes', '/mantenimiento/problemas/subproblemas', 0, 1, NULL, '2025-02-01 20:17:36');
-INSERT INTO `tb_submenu` VALUES (9, 6, 'Menu', 'Config. Menu', '/mantenimiento/menu/menu', 0, 1, NULL, '2025-02-01 20:18:53');
-INSERT INTO `tb_submenu` VALUES (10, 6, 'Sub Menu', 'Config. Menu', '/mantenimiento/menu/submenu', 0, 1, NULL, '2025-02-01 20:19:18');
+INSERT INTO `tb_submenu` VALUES (9, 7, 'Menu', 'Config. Menu', '/mantenimiento/menu/menu', 0, 1, '2025-02-10 15:15:29', '2025-02-01 20:18:53');
+INSERT INTO `tb_submenu` VALUES (10, 7, 'Sub Menu', 'Config. Menu', '/mantenimiento/menu/submenu', 0, 1, '2025-02-10 15:15:44', '2025-02-01 20:19:18');
 
 -- ----------------------------
 -- Table structure for tb_subproblema
@@ -2045,13 +2047,16 @@ CREATE TABLE `tb_vis_asignadas`  (
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `id_visitas`(`id_visitas`) USING BTREE,
   CONSTRAINT `tb_vis_asignadas_ibfk_1` FOREIGN KEY (`id_visitas`) REFERENCES `tb_visitas` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of tb_vis_asignadas
 -- ----------------------------
 INSERT INTO `tb_vis_asignadas` VALUES (2, 3, 3, 1, '2025-01-07', '14:21:40', '2025-01-07 14:21:40');
 INSERT INTO `tb_vis_asignadas` VALUES (3, 5, 4, 1, '2025-01-17', '16:50:53', '2025-01-17 16:50:53');
+INSERT INTO `tb_vis_asignadas` VALUES (4, 4, 1, 1, '2025-02-09', '12:18:00', '2025-02-10 13:03:16');
+INSERT INTO `tb_vis_asignadas` VALUES (5, 6, 4, 1, '2025-02-10', '12:18:00', '2025-02-10 12:18:00');
+INSERT INTO `tb_vis_asignadas` VALUES (6, 6, 3, 1, '2025-02-10', '12:18:00', '2025-02-10 12:18:00');
 
 -- ----------------------------
 -- Table structure for tb_vis_seguimiento
@@ -2093,7 +2098,7 @@ CREATE TABLE `tb_visitas`  (
   INDEX `id_creador`(`id_creador`) USING BTREE,
   CONSTRAINT `tb_visitas_ibfk_1` FOREIGN KEY (`id_sucursal`) REFERENCES `tb_sucursales` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `tb_visitas_ibfk_2` FOREIGN KEY (`id_creador`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of tb_visitas
@@ -2101,6 +2106,7 @@ CREATE TABLE `tb_visitas`  (
 INSERT INTO `tb_visitas` VALUES (3, 121, 1, '2025-01-07', '14:21:40', 0, 0, 0, '2025-01-07 14:21:40');
 INSERT INTO `tb_visitas` VALUES (4, 284, 1, '2025-01-21', '22:56:16', 0, 0, 0, '2025-01-12 22:56:16');
 INSERT INTO `tb_visitas` VALUES (5, 17, 1, '2025-01-21', '16:50:53', 0, 0, 0, '2025-01-17 16:50:53');
+INSERT INTO `tb_visitas` VALUES (6, 121, 1, '2025-02-19', '12:18:00', 0, 0, 0, '2025-02-10 12:18:00');
 
 -- ----------------------------
 -- Table structure for tipo_usuario
@@ -2109,6 +2115,7 @@ DROP TABLE IF EXISTS `tipo_usuario`;
 CREATE TABLE `tipo_usuario`  (
   `id_tipo_acceso` int(11) NOT NULL AUTO_INCREMENT,
   `descripcion` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `color` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   `estatus` tinyint(1) NULL DEFAULT 1,
   PRIMARY KEY (`id_tipo_acceso`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = DYNAMIC;
@@ -2116,10 +2123,10 @@ CREATE TABLE `tipo_usuario`  (
 -- ----------------------------
 -- Records of tipo_usuario
 -- ----------------------------
-INSERT INTO `tipo_usuario` VALUES (1, 'Gerencia', 1);
-INSERT INTO `tipo_usuario` VALUES (2, 'Administrativo', 1);
-INSERT INTO `tipo_usuario` VALUES (3, 'Tecnico', 1);
-INSERT INTO `tipo_usuario` VALUES (4, 'Personalido', 1);
+INSERT INTO `tipo_usuario` VALUES (1, 'Gerencia', 'success', 1);
+INSERT INTO `tipo_usuario` VALUES (2, 'Administrativo', 'info', 1);
+INSERT INTO `tipo_usuario` VALUES (3, 'Tecnico', 'primary', 1);
+INSERT INTO `tipo_usuario` VALUES (4, 'Personalido', 'warning', 1);
 
 -- ----------------------------
 -- Table structure for usuarios
@@ -2166,10 +2173,10 @@ CREATE TABLE `usuarios`  (
 -- ----------------------------
 -- Records of usuarios
 -- ----------------------------
-INSERT INTO `usuarios` VALUES (1, '61505130', 'JEREMY PATRICK', 'CAUPER SILVANO', 'jcauper@gmail.com', 'jcauper@email.com', '2025-02-03 16:02:06', '2003-07-14', '974562354', '954213548', 'jcauper', '$2y$12$tJ1uh20FxN8cWtY8ofWZK.YXJe1d7nVLguPUQxGCEBxzuf8FN.pPG', '123456', 'user_auth.jpg', 'fd_jcauper.png', 2, 1, 'eyIxIjpbXSwiMiI6W10sIjMiOlsiMSIsIjIiXSwiNCI6WyIzIiwiNCIsIjUiXSwiNSI6WyI2Il19', NULL, 0, 1, '2025-02-03 16:02:04', '2024-07-09 23:00:19');
-INSERT INTO `usuarios` VALUES (3, '12345678', 'Pedro', 'Suarez', 'psuarez@gmail.com', 'psuarez@email.com', '2025-02-05 15:08:13', '2003-01-14', '935423118', '952332137', 'psuarez', '$2y$12$dkCCpmvLxzmTkW6OxpZCK.AvBqFpDUVWb90K5EDReDCJ0e3zjeZ5q', '123789', 'fp_psuarez.png', 'fd_psuarez.png', 3, 3, 'eyIyIjpbXX0=', NULL, 0, 1, '2025-02-05 15:08:11', '2024-07-13 02:41:10');
-INSERT INTO `usuarios` VALUES (4, '74716278', 'JOSTHEIN JOSEPH', 'MAYORCA BELLEZA', 'jmayorca@gmail.com', 'jmayorca@email.com', '2025-02-03 14:57:22', '1997-06-11', '978456123', '985267341', 'jmayorca', '$2y$12$XtbOLsPsbk.46I9bhAX2geOJfE7CiCp4YZqweagUQgCbSaUmCHArS', '147852', 'user_auth.jpg', 'fd_jmayorca.png', 3, 1, 'eyIxIjpbXSwiMiI6W119', NULL, 0, 1, '2025-01-28 21:38:10', '2024-07-15 22:18:33');
-INSERT INTO `usuarios` VALUES (5, '70401296', 'BRYAN MARTIN', 'POLO GOMEZ', 'talvan@gmail.com', 'talvan@email.com', '2025-02-03 14:57:22', '2001-07-02', '987564123', '948741236', 'talvan', '$2y$12$h7PwBsMN0k3/57Y3QarvIuHuU1kODsnWbK/zASZz32rY7MYQELKxm', '987654', 'user_auth.jpg', 'fd_talvan.png', 3, 1, 'eyIxIjpbXSwiMiI6W119', NULL, 0, 1, '2025-01-28 21:40:55', '2024-07-22 02:16:27');
+INSERT INTO `usuarios` VALUES (1, '61505130', 'JEREMY PATRICK', 'CAUPER SILVANO', 'jcauper@gmail.com', 'jcauper@email.com', '2025-02-10 16:39:51', '2003-07-14', '974562354', '954213548', 'jcauper', '$2y$12$g3rXIqIc03QRA12UIxy7j.Q3dGINNkz/A45UF.vO7zkhJx.fBkx9G', '123456', 'user_auth.jpg', 'fd_jcauper.png', 4, 1, 'eyI1IjpbIjYiXSwiNyI6WyI5IiwiMTAiXSwiOCI6W119', NULL, 0, 1, '2025-02-10 16:39:49', '2024-07-09 23:00:19');
+INSERT INTO `usuarios` VALUES (3, '12345678', 'Pedro', 'Suarez', 'psuarez@gmail.com', 'psuarez@email.com', '2025-02-10 16:31:20', '2003-01-14', '935423118', '952332137', 'psuarez', '$2y$12$3CmRGy97YD3R0M5j19rrRO.G6AbM6n26v8y3CPEJI8ca2.bsRSiLC', '123789', 'fp_psuarez.png', 'fd_psuarez.png', 3, 3, 'eyIxIjpbXSwiMiI6W119', NULL, 0, 1, '2025-02-10 16:31:18', '2024-07-13 02:41:10');
+INSERT INTO `usuarios` VALUES (4, '74716278', 'JOSTHEIN JOSEPH', 'MAYORCA BELLEZA', 'jmayorca@gmail.com', 'jmayorca@email.com', '2025-02-10 15:28:26', '1997-06-11', '978456123', '985267341', 'jmayorca', '$2y$12$CAclmFJJoM2plUl48iJsgeRbm8WrDbu8jynetkGuWVVBxGTONEm9C', '147852', 'user_auth.jpg', 'fd_jmayorca.png', 3, 1, 'eyIxIjpbXSwiMiI6W119', NULL, 0, 1, '2025-02-10 15:28:24', '2024-07-15 22:18:33');
+INSERT INTO `usuarios` VALUES (5, '70401296', 'BRYAN MARTIN', 'POLO GOMEZ', 'talvan@gmail.com', 'talvan@email.com', '2025-02-10 15:28:11', '2001-07-02', '987564123', '948741236', 'talvan', '$2y$12$6oyxU4QP06ERy7uIw4t6yeJuW1s6bmft/lUWc9SMosYlyZrHPbwN.', '987654', 'user_auth.jpg', 'fd_talvan.png', 3, 1, 'eyIxIjpbXSwiMiI6W119', NULL, 0, 1, '2025-02-10 15:28:09', '2024-07-22 02:16:27');
 
 -- ----------------------------
 -- Procedure structure for GetCodeInc
